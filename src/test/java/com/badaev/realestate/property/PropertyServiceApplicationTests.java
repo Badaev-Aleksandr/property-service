45f14d69163365b40b6582c2ba04b8d99dba19cb
badae <badaev92aleksandr@gmail.com>
2026-07-23 21:43:10 +0200

    Initial property service

---
+++ src/test/java/com/badaev/realestate/property/PropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badaev.realestate.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
